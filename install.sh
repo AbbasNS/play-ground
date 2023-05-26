74941581e78d45575ab3485cbc18ab1871b7dcd2
abbas-sabra-sonarsource <49131500+abbas-sabra-sonarsource@users.noreply.github.com>
2023-05-26 23:08:29 +0000

    add openjdk

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-sudo apk add --no-cache gcompat
+
+sudo apk add --no-cache gcompat openjdk11-jre
 wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub
 wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.35-r1/glibc-2.35-r1.apk
 apk add glibc-2.35-r1.apk
